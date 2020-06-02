a73770acced0c9543956541196428a6bfac92ec3
Dennis C Hackethal <dennis.hackethal@gmail.com>
2020-06-01 20:05:26 -0700

    Run primitives through parser in test

--- test/parser.js
+++ test/parser.js
@@ -4,45 +4,52 @@ let _ = require("lodash");
 
 describe("Parser", () => {
   describe("primitives", () => {
-    describe("arrays", () => {
-      let tokens = [
-        ["[", "["],
-        ["number", "1"],
-        ["number", "2"],
-        ["number", "3"],
-        ["[", "["],
-        ["number", "4"],
-        ["]", "]"],
-        ["number", "5"],
-        ["]", "]"]
-      ];
-      let result = parse(tokens)[0];
+    let tokens = [["number", "1"], ["string", "foo"]];
+    let result = parse(tokens)[0];
 
-      it("parses nested arrays", () => {
-        assert(_.isEqual(result, [
+    it("leaves primitives untouched", () => {
+      assert(_.isEqual(result, tokens));
+    });
+  });
+
+  describe("arrays", () => {
+    let tokens = [
+      ["[", "["],
+      ["number", "1"],
+      ["number", "2"],
+      ["number", "3"],
+      ["[", "["],
+      ["number", "4"],
+      ["]", "]"],
+      ["number", "5"],
+      ["]", "]"]
+    ];
+    let result = parse(tokens)[0];
+
+    it("parses nested arrays", () => {
+      assert(_.isEqual(result, [
+        [
+          "array-literal",
           [
-            "array-literal",
+            ["number", "1"],
+            ["number", "2"],
+            ["number", "3"],
             [
-              ["number", "1"],
-              ["number", "2"],
-              ["number", "3"],
+              "array-literal",
               [
-                "array-literal",
-                [
-                  ["number", "4"]
-                ]
-              ],
-              ["number", "5"],
-            ]
+                ["number", "4"]
+              ]
+            ],
+            ["number", "5"],
           ]
-        ]));
-      });
+        ]
+      ]));
+    });
 
-      it("throws for an unmatched closing bracket", () => {
-        assert.throws(() => {
-          parse([["[", "["], ["]", "]"], ["]", "]"]]);
-        }, /^Unmatched closing bracket ]$/);
-      });
+    it("throws for an unmatched closing bracket", () => {
+      assert.throws(() => {
+        parse([["[", "["], ["]", "]"], ["]", "]"]]);
+      }, /^Unmatched closing bracket ]$/);
     });
   });
 });
