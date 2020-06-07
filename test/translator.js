dc946097b737f71caa140f6edd5fbe3a9cb03f08
Dennis C Hackethal <dennis.hackethal@gmail.com>
2020-06-06 19:48:10 -0700

    Fix tests
    I must have not fixed them last night when I switched the end-of-line logic...

--- test/translator.js
+++ test/translator.js
@@ -21,8 +21,7 @@ describe("Translator", () => {
     let result = translate(ast);
 
     it("converts the ast into a js function call", () => {
-      assert.equal(result, `foo;
-(bar ,\`baz\` ) `);
+      assert.equal(result, `foo (bar ,\`baz\` );\n`);
     });
   });
 
@@ -31,7 +30,7 @@ describe("Translator", () => {
     let result = translate(ast);
 
     it("converts keywords into js symbols", () => {
-      assert.equal(result, "Symbol.for(\"foo-bar!\") ");
+      assert.equal(result, "Symbol.for(\"foo-bar!\");\n");
     });
   });
 
@@ -47,7 +46,7 @@ describe("Translator", () => {
           "__POUND____DOLLAR____PERCENT____CARET__" +
           "__AMPERSAND____ASTERISK____EQUALS____QUOTE__" +
           "__QUESTION_MARK____FORWARD_SLASH____BACKSLASH__" +
-          "__LESS_THAN____GREATER_THAN__bar ");
+          "__LESS_THAN____GREATER_THAN__bar;\n");
       });
     });
 
@@ -56,7 +55,7 @@ describe("Translator", () => {
       let result = translate(ast);
 
       it("does not change the symbol literal", () => {
-        assert.equal(result, "foo_bar ");
+        assert.equal(result, "foo_bar;\n");
       });
     });
   });
@@ -66,7 +65,7 @@ describe("Translator", () => {
     let result = translate(ast);
 
     it("surrounds the string with back ticks", () => {
-      assert.equal(result, "`foo_bar` ");
+      assert.equal(result, "`foo_bar`;\n");
     });
   });
 
@@ -75,7 +74,7 @@ describe("Translator", () => {
     let result = translate(ast);
 
     it("does not touch numbers", () => {
-      assert.equal(result, "12 ");
+      assert.equal(result, "12;\n");
     });
   });
 
@@ -100,7 +99,7 @@ describe("Translator", () => {
     let result = translate(ast);
 
     it("translates nested arrays", () => {
-      assert.equal(result, `[1 ,2 ,3 ,[4 ] ,5 ] `);
+      assert.equal(result, `[1 ,2 ,3 ,[4 ] ,5 ];\n`);
     });
   });
 });
